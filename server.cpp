1cf06380de0838e7fdf35f542733f272c5347d3f
Sergei <tr4shslash@yandex.ru>
2024-02-02 21:06:50 +0300

    serverReady

---
+++ server.cpp
@@ -0,0 +1,48 @@
+#include "server.h"
+#include <QTcpServer>
+#include <QTcpSocket>
+
+
+Server::Server(QObject *parent)
+    : QObject{parent}
+{
+    server_ptr = new QTcpServer;
+    connect(server_ptr, &QTcpServer::newConnection,
+            this, &Server::onNewConnection);
+    if (!server_ptr->listen(QHostAddress::Any, 1234)){
+        qDebug() << "Listen failed";
+        exit(1);
+    }
+    qDebug() << "Listen success";
+}
+
+void Server::onNewConnection()
+{
+    auto clsocket = server_ptr->nextPendingConnection();
+    clients.push_back(clsocket);
+    connect(clsocket, &QTcpSocket::readyRead,
+            this, &Server::onNewMessage);
+    connect(clsocket, &QTcpSocket::disconnected,
+            this, &Server::onDisconnect);
+}
+
+void Server::onDisconnect(){
+    auto socket_ptr = dynamic_cast<QTcpSocket*>(sender());
+    socket_ptr->close();
+    clients.removeOne(socket_ptr);
+    qDebug() << "Client: ";
+}
+
+void Server::onNewMessage(){
+    auto socket_ptr = dynamic_cast<QTcpSocket*>(sender());
+    while(socket_ptr->bytesAvailable() > 0){
+        auto msg = socket_ptr->readAll();
+        qDebug() << msg;
+        for (const auto& cl: clients){
+            if (cl != socket_ptr){
+                cl->write(msg);
+            }
+        }
+    }
+}
+
---
+++ server.h
@@ -0,0 +1,22 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QObject>
+class QTcpServer;
+
+class Server : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Server(QObject *parent = nullptr);
+
+private:
+    QTcpServer* server_ptr;
+    QVector<class QTcpSocket*> clients;
+private slots:
+    void onNewConnection();
+    void onDisconnect();
+    void onNewMessage();
+};
+
+#endif // SERVER_H
