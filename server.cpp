e2e0f6b8688a6094e30e9f1f2d0e32cf4dc71626
Sergei <tr4shslash@yandex.ru>
2024-03-03 17:40:13 +0300

    userlist is supported

--- server.cpp
+++ server.cpp
@@ -28,6 +28,7 @@ Server::Server(QObject *parent)
     }
 }
 
+
 void Server::onNewConnection()
 {
     auto clsocket = server_ptr->nextPendingConnection();
@@ -41,7 +42,7 @@ void Server::onNewConnection()
     query.exec("SELECT * FROM messages");
     QJsonArray arr;
     QString name;
-    QString message;
+    QString message;//рассылаем все прошлые сообщения новому пользователю
     while (query.next()){
         QJsonObject list;
         name = query.value(1).toString();
@@ -51,13 +52,31 @@ void Server::onNewConnection()
         arr.push_back(list);
     }
     QJsonObject json;
+    json.insert("mode", "message");
     json.insert("messages", arr);
-    QJsonDocument doc(json);
-    QString msg = doc.toJson(QJsonDocument::Compact);
-    clsocket->write(msg.toUtf8());
-
+    QJsonArray arr2;
+    auto lst = clients.values();
+    for(const auto& i: lst){
+        arr2.append(i);
+    }
+    QJsonObject userlist;
+    userlist.insert("mode", "add_user");
+    userlist.insert("names", arr2);
+    QJsonObject content;//рассылаем имена пользователей в сети новому пользователю
+    QJsonArray contarr;
+    contarr.append(json);
+    contarr.append(userlist);
+    QJsonObject obj;
+    obj.insert("contents", contarr);
+    QJsonDocument doc(obj);
+    QString str = doc.toJson(QJsonDocument::Compact);
+    clsocket->write(str.toUtf8());
 }
-
+// current messages struct
+//     {"contents": [
+//         {mode: ""...}
+//         ...
+// ]}
 void Server::onDisconnect(){
     auto socket_ptr = dynamic_cast<QTcpSocket*>(sender());
     qDebug() << "Client: " << clients[socket_ptr] << " has left";
@@ -70,28 +89,51 @@ void Server::onNewMessage(){
     while(socket_ptr->bytesAvailable() > 0){
         auto msg = socket_ptr->readAll();
         QJsonDocument doc = QJsonDocument::fromJson(msg);
-        QJsonObject json = doc.object();
-
-        if (json["mode"].toString() == "setName"){
-            clients[socket_ptr] = json["name"].toString();
-            return;
-        }
-        QJsonArray messages = json["messages"].toArray();
-            for (const auto& i: messages){
-            QJsonObject newobj = i.toObject();
-            QSqlQuery query;
-            query.prepare("INSERT INTO messages (name, message) VALUES (:name, :message)");
-            query.bindValue(":name", newobj["name"].toString());
-            query.bindValue(":message", newobj["message"].toString());
-            if (!query.exec()){
-                qDebug() << query.lastError().text();
-            }
-            qDebug() << newobj["name"].toString() << ": " << newobj["message"].toString();
-            for (auto cl = clients.keyBegin(); cl != clients.keyEnd(); ++cl){
-                (*cl)->write(msg);
-
+        QJsonObject content = doc.object();
+        QJsonArray contents = content["contents"].toArray();
+        for(size_t i = 0; i < contents.size(); ++i){
+            QJsonObject json = contents[i].toObject();
+            if (json["mode"].toString() == "setName"){
+                clients[socket_ptr] = json["name"].toString();
+                QJsonArray arr;
+                arr.append(json["name"].toString());
+                QJsonObject userlist;
+                userlist.insert("mode", "add_user");
+                userlist.insert("names", arr);
+                QJsonObject content;
+                QJsonArray contarr;
+                contarr.append(userlist);
+                QJsonObject obj;
+                obj.insert("contents", contarr);
+                QJsonDocument doc(obj);
+                QString str = doc.toJson(QJsonDocument::Compact);
+                for (auto cl = clients.keyBegin(); cl != clients.keyEnd(); ++cl){
+                    if(*cl != socket_ptr){
+                        (*cl)->write(str.toUtf8());
+                        qDebug() << "name sent";
+                    }
+                }
+            }else{
+                QJsonArray messages = json["messages"].toArray();
+                for (const auto& i: messages){
+                    QJsonObject newobj = i.toObject();
+                    QSqlQuery query;
+                    query.prepare("INSERT INTO messages (name, message) VALUES (:name, :message)");
+                    query.bindValue(":name", newobj["name"].toString());
+                    query.bindValue(":message", newobj["message"].toString());
+                    if (!query.exec()){
+                        qDebug() << query.lastError().text();
+                    }
+                    qDebug() << newobj["name"].toString() << ": " << newobj["message"].toString();
+                    for (auto cl = clients.keyBegin(); cl != clients.keyEnd(); ++cl){
+                        (*cl)->write(msg);
+                        qDebug() << "message sent";
+                    }
+                }
             }
         }
     }
 }
+
+
 
